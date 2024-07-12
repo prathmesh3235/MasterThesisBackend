88970279ac2ed7bdd97774fe33b5b72c210e4ef7
Sahil Nair <sahiln@mail.uni-paderborn.de>
2024-07-12 17:07:34 +0200

    addded update to phase number

--- routes/phases.js
+++ routes/phases.js
@@ -33,16 +33,27 @@ router.get('/:id', async (req, res) => {
 // Endpoint to update the title of a specific phase by ID
 router.patch('/:id', async (req, res) => {
   const { id } = req.params;
-  const { title } = req.body;
+  const { title, phaseNo } = req.body;
+
   try {
-    const [results] = await database.query('UPDATE phases SET title = ? WHERE id = ?', [title, id]);
-    if (results.affectedRows === 0) {
-      return res.status(404).json({ message: "Phase not found." });
+    if (title) {
+      const [titleResults] = await database.query('UPDATE phases SET title = ? WHERE id = ?', [title, id]);
+      if (titleResults.affectedRows === 0) {
+        return res.status(404).json({ message: "Phase not found." });
+      }
     }
-    res.json({ message: "Phase title updated successfully." });
+
+    if (phaseNo) {
+      const [phaseNoResults] = await database.query('UPDATE phases SET phaseNo = ? WHERE id = ?', [phaseNo, id]);
+      if (phaseNoResults.affectedRows === 0) {
+        return res.status(404).json({ message: "Phase not found." });
+      }
+    }
+
+    res.json({ message: "Phase updated successfully." });
   } catch (error) {
     console.error("Database error:", error);
-    res.status(500).json({ message: "Failed to update phase title." });
+    res.status(500).json({ message: "Failed to update phase." });
   }
 });
 
